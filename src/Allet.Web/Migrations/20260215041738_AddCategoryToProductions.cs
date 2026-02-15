fb7b00247da6833a2000f0d9946d725de1d8fc79
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2026-02-15 05:19:17 +0100

    fix

---
+++ src/Allet.Web/Migrations/20260215041738_AddCategoryToProductions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Allet.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToProductions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "category",
+                table: "productions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "tags",
+                table: "productions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "tags",
+                table: "productions");
+
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "productions");
+        }
+    }
+}
