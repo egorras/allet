8aee69b9c0bfed7d70deeb3b89e885c5e558c163
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2026-02-15 04:33:52 +0100

    fix

--- src/Allet.Web/Program.cs
+++ src/Allet.Web/Program.cs
@@ -42,12 +42,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
+app.UseHangfireDashboard("/hangfire");
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
-
-app.UseHangfireDashboard("/hangfire");
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
